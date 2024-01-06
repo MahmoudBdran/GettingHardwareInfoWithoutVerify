d0f623808b70b0136729cfa5e3685bb3bc7631c8
MahmoudBdran <41477881+MahmoudBdran@users.noreply.github.com>
2024-01-06 20:17:35 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace gettingHardwareInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cpu_tb = new System.Windows.Forms.TextBox();
+            this.Motherboard_tb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Hard_tb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reload_btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(171, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cpu_ID";
+            // 
+            // cpu_tb
+            // 
+            this.cpu_tb.Location = new System.Drawing.Point(229, 62);
+            this.cpu_tb.Name = "cpu_tb";
+            this.cpu_tb.Size = new System.Drawing.Size(293, 20);
+            this.cpu_tb.TabIndex = 3;
+            // 
+            // Motherboard_tb
+            // 
+            this.Motherboard_tb.Location = new System.Drawing.Point(229, 120);
+            this.Motherboard_tb.Name = "Motherboard_tb";
+            this.Motherboard_tb.Size = new System.Drawing.Size(293, 20);
+            this.Motherboard_tb.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "MotherBoardID";
+            // 
+            // Hard_tb
+            // 
+            this.Hard_tb.Location = new System.Drawing.Point(229, 190);
+            this.Hard_tb.Name = "Hard_tb";
+            this.Hard_tb.Size = new System.Drawing.Size(293, 20);
+            this.Hard_tb.TabIndex = 7;
+            this.Hard_tb.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "HardDiskDriveSerial";
+            this.label3.Click += new System.EventHandler(this.label3_Click_1);
+            // 
+            // reload_btn
+            // 
+            this.reload_btn.Location = new System.Drawing.Point(712, 13);
+            this.reload_btn.Name = "reload_btn";
+            this.reload_btn.Size = new System.Drawing.Size(75, 23);
+            this.reload_btn.TabIndex = 13;
+            this.reload_btn.Text = "Reload";
+            this.reload_btn.UseVisualStyleBackColor = true;
+            this.reload_btn.Click += new System.EventHandler(this.reload_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reload_btn);
+            this.Controls.Add(this.Hard_tb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Motherboard_tb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cpu_tb);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox cpu_tb;
+        private System.Windows.Forms.TextBox Motherboard_tb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Hard_tb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button reload_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gettingHardwareInfo
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private string getProccesorId()
+        {
+            string txthardwareID = "";
+            ManagementObjectSearcher searchinfo = new System.Management.ManagementObjectSearcher("Select * from Win32_Processor");
+            foreach (var i in searchinfo.Get())
+            {
+                txthardwareID = i["ProcessorID"].ToString();
+            }
+            return txthardwareID;
+        }
+        private string getHardDiskDriveSerial()
+        {
+            string temp = "temp";
+            ManagementObjectSearcher searcher = new
+   ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia");
+
+            int i = 0;
+            foreach (ManagementObject wmi_HD in searcher.Get())
+            {
+                // get the hard drive from collection
+                // using index
+
+                // get the hardware serial no.
+                if (wmi_HD["SerialNumber"] == null)
+                {
+                    return "None";
+                }
+
+                else
+                {
+                    return wmi_HD["SerialNumber"].ToString();
+                }
+
+                ++i;
+            }
+            return temp;
+        }
+        private string getMotherboardSerial()
+        {
+            string txthardwareID = "";
+            ManagementObjectSearcher searchinfo = new System.Management.ManagementObjectSearcher("Select * from Win32_BaseBoard");
+            foreach (var i in searchinfo.Get())
+            {
+                txthardwareID = i["SerialNumber"].ToString();
+
+            }
+            return txthardwareID;
+        }
+        public string EncryptSerialKey()
+        {
+            string cpu_id = getProccesorId();
+            string baseboard_id = getMotherboardSerial();
+            string harddiskdrive_id = getHardDiskDriveSerial();
+            string encryptedstring = AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", cpu_id + baseboard_id + harddiskdrive_id);
+            return encryptedstring;
+
+
+
+        }
+        public string EncryptSerialKeyFromGivenValues()
+        {
+            string cpu_id = getProccesorId();
+            string baseboard_id = getMotherboardSerial();
+            string harddiskdrive_id = getHardDiskDriveSerial();
+            string encryptedstring = AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", cpu_tb.Text + Motherboard_tb.Text + Hard_tb.Text);
+            return encryptedstring;
+
+
+
+        }
+        public string DecryptSerialKey()
+        {
+            string cpu_id = getProccesorId();
+            string baseboard_id = getMotherboardSerial();
+            string harddiskdrive_id = getHardDiskDriveSerial();
+            string decryptedstring = AesOperation.DecryptString("b14ca5898a4e4133bbce2ea2315a1916", AesOperation.EncryptString("b14ca5898a4e4133bbce2ea2315a1916", cpu_id + baseboard_id + harddiskdrive_id));
+            return decryptedstring;
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cpu_tb.Text = getProccesorId();
+            Motherboard_tb.Text = getMotherboardSerial();
+            Hard_tb.Text = getHardDiskDriveSerial();
+           // EncryptionKey_tb.Text = "b14ca5898a4e4133bbce2ea2315a1916"; 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+ 
+
+        private void reload_btn_Click(object sender, EventArgs e)
+        {
+
+            Form1 form1 = new Form1();
+         
+            form1.ShowDialog();
+
+        }
+
+      
+    }
+}
